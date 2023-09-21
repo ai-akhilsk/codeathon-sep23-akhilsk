87477d4cab34309ae454ea68121028cf68f0cd36
Akhil S K <129484541+ai-akhilsk@users.noreply.github.com>
2023-09-21 15:16:12 +0530

    program-4 issue-4 commit

---
+++ program-4/program-4.js
@@ -0,0 +1,8 @@
+/**
+ * Sorts an array of numbers in ascending order.
+ * @param {number[]} arr - The array to sort.
+ * @returns {number[]} The sorted array.
+ */
+function sortArray(arr) {
+    return arr.sort((a, b) => a - b);
+  }
